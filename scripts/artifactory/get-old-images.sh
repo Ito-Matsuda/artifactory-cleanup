7e82d5dcb2160ecfea684462cf7b6bc7738494bd
Jose Matsuda <ito-matsuda@hotmail.com>
2021-04-01 11:35:45 -0400

    preparation get-old-images template completed

---
+++ scripts/artifactory/get-old-images.sh
@@ -0,0 +1,14 @@
+# Base example of curling a rest endpoint 
+# curl https://official-joke-api.appspot.com/random_joke >>  joke.txt
+# {"id":222,"type":"general","setup":"What do you call a monkey in a mine field?","punchline":"A babooooom!"}
+
+#Source https://www.jfrog.com/confluence/display/JFROG/Artifactory+REST+API#ArtifactoryRESTAPI-DeleteItem
+#EXAMPLE Using an access token 
+#curl -u myUser:<Token> -X PUT "http://localhost:8081/artifactory/my-repository/my/new/artifact/directory/file.txt" -T Desktop/myNewFile.txt
+
+
+#The AQL is exposed by a REST API https://www.jfrog.com/confluence/display/JFROG/Artifactory+Query+Language
+# old-images-request is what we will send
+
+#obvious fill in the blanks here, something like this probably 
+curl -u myUser:<Token> -X POST -d @old-images-request "someurlhere" >> imagelist.json 
--- scripts/get-old-images.sh
+++
@@ -1,3 +0,0 @@
-# EXAMPLE 
-# curl https://official-joke-api.appspot.com/random_joke >>  joke.txt
-# {"id":222,"type":"general","setup":"What do you call a monkey in a mine field?","punchline":"A babooooom!"}

9fa3d6507457ba8ae7b515d87fedc82f040479ce
Jose-Matsuda <jose.matsuda@canada.ca>
2022-03-18 12:01:16 -0400

    crane

--- 2022/1-RuntimeScanning/b-pull.sh
+++ 2022/1-RuntimeScanning/b-pull.sh
@@ -11,22 +11,24 @@
 # Extra Info
 ### each line in text file should represent an image looking like k8s...io/jupyterlab-cpu:f25cad42
 ### The artifactory remote repository should share the same name as the ACR
-### docker pull jfrog.aaw.cloud.statcan.ca/k8s...io/jupyterlab-cpu:f25cad42
+### crane pull jfrog.aaw.cloud.statcan.ca/k8s...io/jupyterlab-cpu:f25cad42
 ### In the event that the output does not match what we expect break after 10 sleeps
 ###############################################
 
-# https://docs.docker.com/engine/security/rootless/
-
-count=0
 while IFS= read -r line; do
-  until docker pull jfrog.aaw.cloud.statcan.ca/$line 2>&1 | grep -m 1 "Pulling from\|Error response from daemon:"
-  do
-    sleep 1
-    ((count++))
-    if [ $count -eq 10 ]
-      then
-      break
-    fi
-  done
-  count=0
+  crane pull jfrog.aaw.cloud.statcan.ca/$line temporary 
+  rm temporary
 done < a-uniqe-nb-images.txt
+
+
+## TEST, this doesnt work super well
+#until crane pull jfrog.aaw.cloud.statcan.ca/jose-play/busybox:stable temporary 2>&1 \
+#  | grep -m 1 "(compressed) in cache\|Forbidden"
+#  do
+#    sleep 1
+#    ((count++))
+#    if [ $count -eq 10 ]
+#      then
+#      break
+#    fi
+#  done
